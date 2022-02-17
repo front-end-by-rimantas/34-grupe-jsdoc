c094a25ce8c31193d0fd05cd1b7d7fd66686c6ee
Rimantas <belauzas@yahoo.com>
2022-02-17 19:44:04 +0200

    Dokumentacija

---
+++ index.js
@@ -0,0 +1,11 @@
+const sum = require("./js/sum.js");
+const chat = require("./js/chat.js");
+const studentAverage = require("./js/studentAverage.js");
+
+const pirmas = 7;
+const antras = 5;
+const r = sum(pirmas, antras);
+
+const petrasLabas = chat('Petras', 'labas');
+
+const m = studentAverage('Ona', [10, 2, 8, 4]);

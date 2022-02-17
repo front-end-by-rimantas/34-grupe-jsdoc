579e0bf8f6c0de1f1b8db919d05a8b150065c7e2
Rimantas <belauzas@yahoo.com>
2022-02-17 21:22:17 +0200

    Car task solution

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ const volvo = car('Volvo XC90', 60, 6, 7, 8);
 
 // Volvo XC90 su 60l baku, gali nuvaziuoti 1000km (magistralej, kai sanaudos yra 6l/100km), 857km (vidutiniskai, kai sanaudos yra 7l/100km) ir 750km (mieste, kai sanaudos yra 8l/100km).
 console.log(volvo);
+
+const audi = car('Audi 80', 50, 8, 10, 12);
+
+// Audi 80 su 50l baku, gali nuvaziuoti 625km (magistralej, kai sanaudos yra 8l/100km), 500km (vidutiniskai, kai sanaudos yra 10l/100km) ir 416km (mieste, kai sanaudos yra 12l/100km).
+console.log(audi);
--- js/car.js
+++ js/car.js
@@ -1,5 +1,30 @@
-function car() {
-    return;
+/**
+ * Masinos degalu sanaudu charakteristikos priklausomai nuo vaziavimo aplinkybiu.
+ * @param {string} name Automobilio pavadinimas
+ * @param {number} volume Degalu bako talpa litrais
+ * @param {number} consuption1 Degalu sanaudos magistraleje 100-ui km
+ * @param {number} consuption2 Degalu vidutines sanaudos 100-ui km
+ * @param {number} consuption3 Degalu sanaudos mieste 100-ui km
+ * @returns {string} Sakinys
+ */
+function car(name, volume, consuption1, consuption2, consuption3) {
+    const dist = 100;
+    const distance1 = distance(volume, consuption1);
+    const distance2 = distance(volume, consuption2);
+    const distance3 = distance(volume, consuption3);
+
+    return `${name} su ${volume}l baku, gali nuvaziuoti ${distance1}km (magistralej, kai sanaudos yra ${consuption1}l/${dist}km), ${distance2}km (vidutiniskai, kai sanaudos yra ${consuption2}l/${dist}km) ir ${distance3}km (mieste, kai sanaudos yra ${consuption3}l/${dist}km).`;
+}
+
+/**
+ * Vidutinio atstumo apskaiciavimas pagal kuro bako dydi ir kuro sanaudas
+ * @param {number} volume Kuro bako talpa
+ * @param {number} consuption Kuro sanaudos 100-ui km
+ * @param {number} distance Kokiam atstumui skaiciuojamos kuro sanaudos
+ * @returns 
+ */
+function distance(volume, consuption, distance = 100) {
+    return Math.floor(volume * distance / consuption);
 }
 
 module.exports = car;
